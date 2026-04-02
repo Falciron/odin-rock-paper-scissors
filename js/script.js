9e46935a85e33e15501681d09d5f5029dcac9051
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-02 14:48:42 -0700

    Add a function to get the computer's choice
    Rock paper scissors is a game about throwing hands
    so make sure the computer has some to use.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+/**
+ * This game will be played against the computer. Its choice will be randomly selected between "rock", "paper", and "scissors".
+ * To facilitate this, let's make a function that takes no inputs and returns one of the three strings.
+ * 
+ * WHEN THIS FUNCTION IS CALLED
+ * GET A NUMBER AT RANDOM BETWEEN 0 AND 2
+ * MATCH THAT NUMBER TO ROCK, PAPER, and SCISSORS
+ * RETURN THAT WORD
+ */
+
+function getComputerChoice(){
+    const randomChoiceNumber = Math.floor(3 * Math.random());
+    switch (randomChoiceNumber){
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Scissors";
+            break;
+        default:
+            return "Paper";
+    }
+}
