99bf51a6dbd88245ea8626fc228c431c52ed5cc7
Falciron <2159742+Falciron@users.noreply.github.com>
2026-04-08 16:44:07 -0700

    Display the running score and five game winner
    The final information that we need to move out of
    the console is the announcement of the final
    winner. Testing the full game loop exposed a
    few other issues, so this commit is a bit larger
    than most.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,4 @@
 /**
- * Determines the computer's hand selection using random number generation.
- * @returns {"rock"|"paper"|"scissors"} The computer's choice of hand, represented as a string.
- */
-function getComputerChoice() {
-    const randomChoiceNumber = Math.floor(3 * Math.random());
-    switch (randomChoiceNumber) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        default:
-            return "scissors";
-    }
-}
-
-/**
- * Obtains the player's hand selection using a visual prompt.
- * @returns {string | null} The player's choice of hand, represented as a string, or null, if the player cancels the prompt.
- */
-function getHumanChoice() {
-    return prompt("Please type either 'rock', 'paper', or 'scissors' then press OK.");
-}
-
-/**
  * Facilitates a game of Rock, Paper, Scissors consisting of five rounds, calculates the winner/tie, then displays the final results.
  */
 function playGame() {
@@ -30,71 +6,114 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
+    // Preemptively query the elements that the code will regularly interact with.
+    const handButtons = document.querySelectorAll("button");
     const roundResults = document.querySelector("#roundResults");
+    const scoreSpan = document.querySelector("#scoreSpan");
 
-    function registerRoundResult(newRoundString){
-        const newResult = document.createElement("p");
-        newResult.textContent = newRoundString;
-        roundResults.append(newResult);
+    // Start a round by clicking one of the three hand buttons.
+    handButtons.forEach(button => button.addEventListener("click", playRound));
+
+    /**
+     * Determines the computer's hand selection using random number generation.
+     * @returns {"Rock"|"Paper"|"Scissors"} The computer's choice of hand, represented as a string.
+     */
+    function getComputerChoice() {
+        const randomChoiceNumber = Math.floor(3 * Math.random());
+        switch (randomChoiceNumber) {
+            case 0:
+                return "Rock";
+            case 1:
+                return "Paper";
+            default:
+                return "Scissors";
+        }
     }
 
     /**
-     * Facilitates a single round of Rock, Paper, Scissors, calculates the winner/tie, announces the result, then updates the scores.
+     * Adds a new entry to the list of round results, then updates the scores.
+     * @param {string} roundResultText Description of the hands thrown and round result
+     */
+    function registerRoundResults(roundResultText) {
+        const newResult = document.createElement("p");
+        newResult.textContent = roundResultText;
+        roundResults.prepend(newResult);
+
+        updateScoreDisplay();
+    }
+
+    /**
+     * Refreshes the player and computer scores so they contain up to date results.
+     */
+    function updateScoreDisplay() {
+        scoreSpan.textContent = `Player: ${humanScore} | Computer: ${computerScore}`;
+    }
+
+    /**
+     * Removes the buttons to avoid further rounds being played, then announces the final winner and entices the player to restart.
+     * @param {string} gameResultText Details of the game winner and instructions to restart
+     */
+    function endGame(gameResultText) {
+        const scoreLabel = document.querySelector("#scoreLabel");
+        const gameResult = document.querySelector("#gameResult");
+
+        handButtons.forEach(button => button.remove());
+
+        gameResult.style.fontWeight = "bold";
+        gameResult.textContent = gameResultText;
+
+        scoreLabel.textContent = "Final Score";
+    }
+
+    /**
+     * Starts a single round of Rock, Paper, Scissors, beginning with the player choice, 
+     * then calculates the winner/tie, announces the result, and updates the scores.
+     * If either player reaches a score of five points, then the game is concluded.
      * The game of Rock, Paper, Scissors uses the following logic to determine a winner:
      * Rock wins vs. Scissors, Scissors wins vs. Paper, and Paper wins vs. Rock
-     * @param {"rock" | "paper" | "scissors"} humanChoice 
-     * @param {"rock" | "paper" | "scissors"} computerChoice 
+     * @param {PointerEvent} event A click event from the human player clicking one of the three buttons
      */
-    const playRound = (event) => {
+    function playRound(event) {
         const computerChoice = getComputerChoice();
-        const humanChoice = event.currentTarget.textContent.toLowerCase();
+        const humanChoice = event.currentTarget.textContent;
         let roundResult;
 
         switch (humanChoice) {
-            case "rock":
-                if (computerChoice === "scissors") roundResult = "humanWin";
-                else if (computerChoice === "paper") roundResult = "humanLoss";
+            case "Rock":
+                if (computerChoice === "Scissors") roundResult = "humanWin";
+                else if (computerChoice === "Paper") roundResult = "humanLoss";
                 break;
 
-            case "paper":
-                if (computerChoice === "rock") roundResult = "humanWin";
-                else if (computerChoice === "scissors") roundResult = "humanLoss";
+            case "Paper":
+                if (computerChoice === "Rock") roundResult = "humanWin";
+                else if (computerChoice === "Scissors") roundResult = "humanLoss";
                 break;
 
-            case "scissors":
-                if (computerChoice === "paper") roundResult = "humanWin";
-                else if (computerChoice === "rock") roundResult = "humanLoss";
+            case "Scissors":
+                if (computerChoice === "Paper") roundResult = "humanWin";
+                else if (computerChoice === "Rock") roundResult = "humanLoss";
                 break;
         }
 
         switch (roundResult) {
             case "humanWin":
-                registerRoundResult(`You win! ${humanChoice} beats ${computerChoice}`);
                 humanScore++;
+                registerRoundResults(`You win! ${humanChoice} beats ${computerChoice.toLowerCase()}`);
                 break;
             case "humanLoss":
-                registerRoundResult(`You lose! ${computerChoice} beats ${humanChoice}`);
                 computerScore++;
+                registerRoundResults(`You lose! ${computerChoice} beats ${humanChoice.toLowerCase()}`);
                 break;
             default:
-                registerRoundResult(`It's a tie! ${humanChoice} ties ${computerChoice}`);
+                registerRoundResults(`It's a tie! ${humanChoice} ties ${computerChoice.toLowerCase()}`);
+        }
+
+        if (humanScore === 5) {
+            endGame("Congratulations, you won the match! Refresh the page for a rematch!");
+        } else if (computerScore === 5) {
+            endGame("Sadly, the computer won the match. Refresh the page to seek your revenge!");
         }
     }
-
-    const handButtons = document.querySelectorAll("button");
-    handButtons.forEach(button => button.addEventListener("click", playRound));
-
-    let resultString;
-    if (humanScore > computerScore) {
-        resultString = "Congratulations, you won the match!";
-    } else if (computerScore > humanScore) {
-        resultString = "Sadly, the computer won the match.";
-    } else {
-        resultString = "The match has ended in a tie!";
-    }
-
-    console.log(resultString);
-    alert(resultString);
 }
 
 // Run the game on page load.
